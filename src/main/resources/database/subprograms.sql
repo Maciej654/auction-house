696c789aeca7f27ecedd77fbed179c7829436950
Maciej654 <62801237+Maciej654@users.noreply.github.com>
2021-01-16 23:11:50 +0100

    sql scripts updated

--- src/main/resources/database/subprograms.sql
+++ src/main/resources/database/subprograms.sql
@@ -1,28 +1,24 @@
 CREATE OR REPLACE PACKAGE Subprograms IS
-
-     FUNCTION calculateRating (p_reviewee VARCHAR) RETURN number ;
-
-    PROCEDURE delayAuctionsEnd (p_auction_name VARCHAR, p_seller VARCHAR, p_item_name VARCHAR);
-
+    FUNCTION calculateRating (p_reviewee VARCHAR) RETURN number ;
+    PROCEDURE delayAuctionsEnd (p_id NUMBER);
 END Subprograms;
 
 CREATE OR REPLACE PACKAGE BODY Subprograms IS
     FUNCTION calculateRating (p_reviewee VARCHAR) RETURN number is
         reviewee_rating number;
-        begin
-            select avg(r.rating)
-            into reviewee_rating
-            from ratings r join auctions a on r.auction = a.auction_id
-            where a.seller = p_reviewee;
+    begin
+        select avg(r.rating)
+        into reviewee_rating
+        from ratings r join auctions a on r.auction = a.auction_id
+        where a.seller = p_reviewee;
         return reviewee_rating;
     end calculateRating;
-     PROCEDURE delayAuctionsEnd (p_auction_name VARCHAR, p_seller VARCHAR, p_item_name VARCHAR) IS
-     BEGIN
+
+    PROCEDURE delayAuctionsEnd (p_id number) IS
+    BEGIN
         update auctions
-            set end_date = end_date +  interval '1' day
-        where auction_name = p_auction_name
-            and seller = p_seller
-            and item_name = p_item_name;
-    END DELAYAUCTIONSEND;
+        set end_date = end_date +  interval '1' day
+        where auction_id = p_id;
+    END delayAuctionsEnd;
 
 END Subprograms;
