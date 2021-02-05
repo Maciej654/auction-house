057122c9f8c446ff62f6fd9f0eb386f9f0fab73f
pjaro <p.jaronsky@gmail.com>
2021-02-05 09:23:07 +0100

    adjust end date format

--- src/main/java/pl/poznan/put/util/converter/DateConverterUtils.java
+++ src/main/java/pl/poznan/put/util/converter/DateConverterUtils.java
@@ -8,7 +8,7 @@ import java.time.format.DateTimeFormatter;
 @UtilityClass
 public class DateConverterUtils {
     private final String LOG_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
-    private final String END_DATE_PATTERN = "yyyy-MM-dd HH";
+    private final String END_DATE_PATTERN = "yyyy-MM-dd HH:mm";
 
     private final DateTimeFormatter LOG_DATE_FORMATTER = DateTimeFormatter.ofPattern(LOG_DATE_PATTERN);
     private final DateTimeFormatter END_DATE_FORMATTER = DateTimeFormatter.ofPattern(END_DATE_PATTERN);
